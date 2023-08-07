82602f3680d6e9a6d2670d5e2e3ae9e87313fb94
James Yu Cao <caoyu19920414@gmail.com>
2023-08-06 23:56:48 -0400

    mod read me final 2

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom"
-import {connect} from 'react-redux'
-import {addNewItem,loadInitialData,markItemComplete
-	   ,loadInitialDataSocket,addNewItemSocket,markItemCompleteSocket
-	   ,AddItem,completeItem} from '../actions/action'
+import { connect } from 'react-redux'
+import {
+	addNewItem, loadInitialData, markItemComplete
+	, loadInitialDataSocket, addNewItemSocket, markItemCompleteSocket
+	, AddItem, completeItem
+} from '../actions/action'
 import io from "socket.io-client"
 
 import TextField from 'material-ui/TextField'
 import RaisedButton from 'material-ui/RaisedButton'
-import {List, ListItem} from 'material-ui/List'
+import { List, ListItem } from 'material-ui/List'
 import Divider from 'material-ui/Divider'
 
 // import {List as List} from 'immutable';
@@ -21,78 +23,78 @@ let robotFontStyle = {
 	fontFamily: "Roboto, sans-serif",
 	color: "rgba(0, 0, 0, 0.870588)"
 }
-let markCompleteStyle = {   
-  textDecoration: "line-through"
+let markCompleteStyle = {
+	textDecoration: "line-through"
 }
 let socket
 const mapStateToProps = (state = {}) => {
 	// console.dir(state)
-    return {...state};
+	return { ...state };
 };
 
-export  class Layout extends React.Component{
-   constructor(props)
-   {
-	   super(props)
-	   const {dispatch} = this.props
-	//    dispatch(loadInitialData())
-	   socket = io.connect("http://localhost:3000")
-	   console.dir(socket)
-	   dispatch(loadInitialDataSocket(socket))
-	   
-	   socket.on('itemAdded',(res)=>{
-		   console.dir(res)
-		   dispatch(AddItem(res))
-	   })
+export class Layout extends React.Component {
+	constructor(props) {
+		super(props)
+		const { dispatch } = this.props
+		//    dispatch(loadInitialData())
+		socket = io.connect("http://localhost:3000")
+		console.dir(socket)
+		dispatch(loadInitialDataSocket(socket))
 
-	   socket.on('itemMarked',(res)=>{
-		   console.dir(res)
-		   dispatch(completeItem(res))
-	   })
-   }
+		socket.on('itemAdded', (res) => {
+			console.dir(res)
+			dispatch(AddItem(res))
+		})
 
-   componentWillUnmount() {
-       socket.disconnect()
-	   alert("Disconnecting Socket as component will unmount")
-   }
+		socket.on('itemMarked', (res) => {
+			console.dir(res)
+			dispatch(completeItem(res))
+		})
+	}
 
-   render(){	
-       const {dispatch,items} = this.props
-		
+	componentWillUnmount() {
+		socket.disconnect()
+		alert("Disconnecting Socket as component will unmount")
+	}
+
+	render() {
+		const { dispatch, items } = this.props
+
 		return (
 			<div>
 				<h1 style={robotFontStyle}>React TO-DO (Real-Time)</h1>
-				
-                <Divider/>
-				<TextField 
+
+				<Divider />
+				<TextField
 					hintText="Add New Item"
-      				floatingLabelText="Enter the new item"
+					floatingLabelText="Enter the new item"
 					ref="newTodo"
 				/>
-                {" "}
+				{" "}
 				<RaisedButton
 					label="Click to add!" primary={true}
-					onTouchTap={ () => {
-                        const newItem = ReactDOM.findDOMNode(this.refs.newTodo.input).value
-                        newItem === "" ?  alert("Item shouldn't be blank")
-						               :  dispatch(addNewItemSocket(socket,items.size,newItem)) 
-									    {/*: dispatch(addNewItem(items.size,newItem))*/}
-                        ReactDOM.findDOMNode(this.refs.newTodo.input).value = ""
-					  }
+					onTouchTap={() => {
+						const newItem = ReactDOM.findDOMNode(this.refs.newTodo.input).value
+						newItem === "" ? alert("Item shouldn't be blank")
+							: dispatch(addNewItemSocket(socket, items.size, newItem))
+						{/*: dispatch(addNewItem(items.size,newItem))*/ }
+						ReactDOM.findDOMNode(this.refs.newTodo.input).value = ""
+					}
 					}
 				/>
-				<List>{items.map((todo,key)=>{
-                    return <ListItem key={key} style={todo.completed?markCompleteStyle:{}} onClick={ (event) => {
-									  {/*dispatch(markItemComplete(key+1,!todo.completed))*/}
-									  dispatch(markItemCompleteSocket(socket,key+1,!todo.completed))
-									}
-								} primaryText={todo.item}>
-						   </ListItem>})
+				<List>{items.map((todo, key) => {
+					return <ListItem key={key} style={todo.completed ? markCompleteStyle : {}} onClick={(event) => {
+						{/*dispatch(markItemComplete(key+1,!todo.completed))*/ }
+						dispatch(markItemCompleteSocket(socket, key + 1, !todo.completed))
+					}
+					} primaryText={todo.item}>
+					</ListItem>
+				})
 				}</List>
-				
+
 			</div>
 		);
 	}
 }
 
-export default  connect(mapStateToProps)(Layout)
+export default connect(mapStateToProps)(Layout)
